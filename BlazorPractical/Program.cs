a9c2ff948ff336804a64f7bb7f476455e50c7bed
brianjuma <98020785+briannjuma@users.noreply.github.com>
2024-08-06 23:09:04 +0300

    Add service for person CRUD

--- BlazorPractical/Program.cs
+++ BlazorPractical/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddSingleton<DatabaseContext>();
+builder.Services.AddScoped<PersonService>();
 
 var app = builder.Build();
 
---
+++ BlazorPractical/Services/PersonService.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorPractical.Services;
+
+public class PersonService
+{
+    private readonly DatabaseContext _dbContext;
+
+    public PersonService(DatabaseContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<IEnumerable<Person>> GetPeopleAsync()
+    {
+        var sql = "SELECT PersonId, FirstName, LastName FROM sp_people";
+
+        using (var connection = _dbContext.DatabaseConnection())
+        {
+            return await connection.QueryAsync<Person>(sql);
+        }
+
+    }
+
+
+}
