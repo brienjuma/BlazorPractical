736c5d343a6fd0ba0e117f6d43bae62498685201
brianjuma <98020785+briannjuma@users.noreply.github.com>
2024-08-06 21:29:02 +0300

    Add model Person
    define member properties for person objects

---
+++ BlazorPractical/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorPractical.Models;
+
+public class Person
+{
+    public string PersonId { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
