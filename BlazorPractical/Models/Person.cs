17769ae8016f55b5cdf66ef0e820c2b612319993
brianjuma <98020785+briannjuma@users.noreply.github.com>
2024-08-07 09:33:05 +0300

    Add data validation check
    registration form validates input data at submit

--- BlazorPractical/Models/Person.cs
+++ BlazorPractical/Models/Person.cs
@@ -1,8 +1,17 @@
-﻿namespace BlazorPractical.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorPractical.Models;
 
 public record class Person
 {
+    [Required(ErrorMessage = "Person ID is required")]
     public string PersonId { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "First Name is required")]
+    [StringLength(50, ErrorMessage = "First Name can't be longer than 50 characters")]
     public string FirstName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Last Name is required")]
+    [StringLength(50, ErrorMessage = "Last Name can't be longer than 50 characters")]
     public string LastName { get; set; } = string.Empty;
 }
